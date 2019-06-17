10bdcaf8b3f4212c8568285e468f4924bc838484
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2019-06-17 09:56:38 -0400

    Getting started with Nodejs

---
+++ Nodejs/person.js
@@ -0,0 +1,18 @@
+﻿/*const person = {
+    name: 'Chris',
+    age: 25
+};*/
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age} years old.`);
+    }
+
+}
+
+module.exports = Person;
