00aa87560080fa84435b5500e0bedcc677f930e3
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2019-08-23 15:45:11 -0400

    Add files via upload

---
+++ Nodejs/NodeJS Passport Login/routes/index.js
@@ -0,0 +1,25 @@
+﻿const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const fs = require('fs');
+const budgetData = require('../data.json');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+    res.render('dashboard', {
+        user: req.user,
+        budgetData: budgetData.allItems
+    })
+);
+
+router.route('/post').post((req, res) => {
+    let expenses = req.body;
+    let data = JSON.stringify(expenses);
+    fs.writeFileSync('data.json', data);
+    res.send(req.body);
+});
+
+module.exports = router;
