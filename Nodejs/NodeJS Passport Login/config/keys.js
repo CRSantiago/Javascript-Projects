a2b7da8acab5df8af39d8c130983165c22e281b6
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2019-08-23 15:44:14 -0400

    Add files via upload

---
+++ Nodejs/NodeJS Passport Login/config/keys.js
@@ -0,0 +1,5 @@
+﻿dbPassword = 'mongodb+srv://chris217:' + encodeURIComponent('MyD3-i$-C00l') + '@cscluster01-vwrmn.mongodb.net/budget-app?retryWrites=true&w=majority';
+
+module.exports = {
+    MongoURI: dbPassword
+};
