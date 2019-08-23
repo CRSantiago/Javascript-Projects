d8471107e104d7308358bd42d54ccc8e0c23e1b3
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2019-08-23 15:44:45 -0400

    Add files via upload

---
+++ Nodejs/NodeJS Passport Login/models/User.js
@@ -0,0 +1,24 @@
+﻿const mongoose = require('mongoose');
+
+const UserScheme = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', UserScheme);
+
+module.exports = User;
