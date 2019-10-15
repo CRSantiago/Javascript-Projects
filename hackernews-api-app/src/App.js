7c293d16b97b43b4e52734ab64b62ea83e8dbe33
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2019-10-15 15:01:19 -0400

    feat: news reader

---
+++ hackernews-api-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StoriesContainer} from './containers/StoriesContainer';
+
+export const App = () => {
+
+  return (
+    <div>
+      <StoriesContainer/>
+    </div>
+  );
+}
+
